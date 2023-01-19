5e2b444797ad06e807102be2cfb1207ad7a89bc9
dimaphilipchuk <dimaphilipchuk2014@gmail.com>
2023-01-19 17:54:41 +0200

    Changing variable names in functions/converter_function.py

--- functions/converter_function.py
+++ functions/converter_function.py
@@ -3,23 +3,14 @@
 def converter_voice_msg(audio):
     import subprocess
 
-    CurrentFileName = audio
-    FinalFileName = 'Твоє голосове повідомлення.mp3'
+    current_file_name = audio
+    final_file_name = 'Твоє голосове повідомлення.mp3'
 
     try:
-        subprocess.call(['ffmpeg', '-i', f'{CurrentFileName}', f'{FinalFileName}'])
+        subprocess.call(['ffmpeg', '-i', f'{current_file_name}', f'{final_file_name}'])
 
     except Exception as e:
         print(e)
         print('Error While Converting Audio')
 
-    return FinalFileName
-
-
-
-
-
-
-
-
-
+    return final_file_name
