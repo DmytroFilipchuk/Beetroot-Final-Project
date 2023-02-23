c023a761de8c5dccd30235b149417a9cafe82af2
dimaphilipchuk <dimaphilipchuk2014@gmail.com>
2023-02-23 15:23:47 +0200

    Update in converter_function.py

--- functions/converter_function.py
+++ functions/converter_function.py
@@ -4,10 +4,10 @@ def converter_voice_msg(audio):
     import subprocess
 
     current_file_name = audio
-    final_file_name = 'ConvertedByFFeelMusicBot.mp3'
+    final_file_name = './ConvertedByFFeelMusicBot.mp3'
 
     try:
-        subprocess.call(['ffmpeg', '-i', f'{current_file_name}', f'{final_file_name}'])
+        subprocess.call(['./ffmpeg', '-i', f'{current_file_name}', f'{final_file_name}'])
 
     except Exception as e:
         print(e)
